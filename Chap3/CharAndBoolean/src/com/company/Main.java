3ca63994790e3356722e852cb23b70e1047e47cf
Anjali <anjali.payal69@gmail.com>
2020-05-21 17:52:23 +0200

    Char And Boolean

---
+++ Chap3/CharAndBoolean/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Video 29
+        //CHAR
+        char myChar = 'D';
+        char myUnicodeChar1 = '\u0044';
+        char myUnicodeChar2 = '\u00A9';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar1);
+        System.out.println(myUnicodeChar2);
+
+        //BOOLEAN
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        
+        boolean isCustomerOverTwentyOne = true;
+
+        System.out.println(myTrueBooleanValue);
+        System.out.println(myFalseBooleanValue);
+        System.out.println(isCustomerOverTwentyOne);
+
+    }
+}
