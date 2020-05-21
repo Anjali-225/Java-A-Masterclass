6a09deeeb857dcf31a59062315d19e66b2b13bef
Anjali <anjali.payal69@gmail.com>
2020-05-21 18:50:44 +0200

    Logical_AND_Operator

--- Chap3/Operators/src/com/company/Main.java
+++ Chap3/Operators/src/com/company/Main.java
@@ -53,11 +53,27 @@ public class Main {
         System.out.println("");
 
         //Video 34
+        //If-Then-Statements
         boolean isAlien = false;
         if (isAlien == false) {
             System.out.println("It is not an alien!");
             System.out.println("And I am scared of aliens");
         }
+
+        System.out.println("");
+
+        //Video 35
+        //Logical And Operators
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater that second top score and less than 100");
+        }
+
 
     }
 }
