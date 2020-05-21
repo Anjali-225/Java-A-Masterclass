cf092a6b1060e99d0ca079c88e10714109e31209
Anjali <anjali.payal69@gmail.com>
2020-05-21 11:02:44 +0200

    Hello World

---
+++ Chap3/HelloWorld/src/Hello.java
@@ -0,0 +1,23 @@
+public class Hello {
+    public static void main(String[] args) {
+        //Video 20
+        System.out.println("Hello World!");
+
+        //Video 21
+        System.out.println("Hello Anjali!");
+
+        //Video 22
+        int myFirstNumber = (5 + 10) + (2 *10);
+        System.out.println(myFirstNumber);
+
+        //Video 23
+        int mySecondNumber = 7;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+
+    }
+}
