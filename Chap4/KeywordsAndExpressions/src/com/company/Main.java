ace76b2dbad21a66da103c1910dddcf49768505f
Anjali <anjali.payal69@gmail.com>
2020-05-26 17:27:24 +0200

    KeyWords_State_Express

---
+++ Chap4/KeywordsAndExpressions/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // video 43
+        double kilometers = (100 * 1.609344);
+        System.out.println(kilometers);
+
+        int highScore = 50;
+        if (highScore == 50) {
+            System.out.println("This is an expression!");
+        }
+
+        //Challenge
+        int score = 100;  //Expression ~ score = 100;
+        if (score > 99) { //Expression ~ score > 99
+            System.out.println("you got the highscore"); //Expression ~ "you got the highscore"
+            score = 0; //Expression ~ score = 0;
+        }
+
+    }
+}
---
+++ Chap4/StatementsWhiteSpacesAndIndenting/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //  video 44
+        //Statements
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+        System.out.println("This is" +
+                " another " +
+                " still more");
+
+        int anotherVariable =50; myVariable--;
+        System.out.println("This is another one");
+
+        //Whitespace
+        //Java dont mind space
+        int      newVariable    =    20;
+
+        //Indentation
+        //Its aim is better readability
+        //Makes it easy for you to see
+
+    }
+}
