9743e746273b4b4889e6a80cfa64d46892a4210c
Anjali <anjali.payal69@gmail.com>
2020-06-21 14:49:22 +0200

    Choosers_And_WebPages

--- Chap13_JavaFX/JavaFXApplication/src/module-info.java
+++ Chap13_JavaFX/JavaFXApplication/src/module-info.java
@@ -3,6 +3,9 @@ module JavaFXApplication {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.desktop;
+    requires kotlin.stdlib;
+    requires javafx.web;
 
     opens sample;
 }
