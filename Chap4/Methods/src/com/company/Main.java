10621ede63409e3567554f614df0a61ee5c9fd4d
Anjali <anjali.payal69@gmail.com>
2020-08-26 17:08:25 +0200

    More On Methods

--- Chap4/Methods/src/com/company/Main.java
+++ Chap4/Methods/src/com/company/Main.java
@@ -12,6 +12,18 @@ public class Main {
         System.out.println("Your final score is " + highScore1);
         int highScore2 = calculateScore2(true, 10000, 8, 200);
         System.out.println("Your final score is " + highScore2);
+
+        int highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Bob", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Tim", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Percy", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Greg", highScorePosition);
     }
 
     public static void calculateScore1(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -31,4 +43,22 @@ public class Main {
             return -1;
         }
     }
+
+    // video 48 + Challenge
+    
+    public static void displayHighScorePosition(String playerName, int highScorePosition) {
+        System.out.println(playerName + " managed to get into position " + highScorePosition + " on the high score table");
+    }
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000){
+            return 1;
+        } else if (playerScore >= 500 && playerScore < 1000) {
+            return 2;
+        } else if (playerScore >= 100 && playerScore < 500) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+
 }
