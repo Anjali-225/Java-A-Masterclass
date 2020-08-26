1c85e5d9e86972620250462b407e510be1da982b
Anjali <anjali.payal69@gmail.com>
2020-08-26 17:17:25 +0200

    Methods Challenge

--- Chap4/Methods/src/com/company/Main.java
+++ Chap4/Methods/src/com/company/Main.java
@@ -45,20 +45,35 @@ public class Main {
     }
 
     // video 48 + Challenge
-    
+
     public static void displayHighScorePosition(String playerName, int highScorePosition) {
         System.out.println(playerName + " managed to get into position " + highScorePosition + " on the high score table");
     }
     public static int calculateHighScorePosition(int playerScore) {
+//        if (playerScore >= 1000){
+//            return 1;
+//        } else if (playerScore >= 500 ) {
+//            return 2;
+//        } else if (playerScore >= 100 ) {
+//            return 3;
+//        } else {
+//            return 4;
+//        }
+
+        // video 49
+        
+        int position = 4; //assuming position4 will be returned
+
         if (playerScore >= 1000){
-            return 1;
-        } else if (playerScore >= 500 && playerScore < 1000) {
-            return 2;
-        } else if (playerScore >= 100 && playerScore < 500) {
-            return 3;
-        } else {
-            return 4;
+            position = 1;
+        } else if (playerScore >= 500 ) {
+            position = 2;
+        } else if (playerScore >= 100 ) {
+            position = 3;
         }
+
+        return position;
+
     }
 
 }
