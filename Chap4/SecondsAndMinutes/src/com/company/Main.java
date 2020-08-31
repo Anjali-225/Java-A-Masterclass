9cd562e6861d612f4cb574817558756cbdf95ae4
Anjali <anjali.payal69@gmail.com>
2020-08-31 14:49:47 +0200

    Bonus Challenge Solution

--- Chap4/SecondsAndMinutes/src/com/company/Main.java
+++ Chap4/SecondsAndMinutes/src/com/company/Main.java
@@ -12,10 +12,28 @@ public class Main {
 
         if ((minutes < 0) || ((seconds < 0) || (seconds > 59))) {
             return "Invalid Value";
+
         } else {
+
             long hours = minutes / 60;
             long remainingMinutes = minutes % 60;
-            return hours + "h " + remainingMinutes + "m " + seconds + "s";
+
+            String hoursString = hours + "h ";
+            if (hours < 10) {
+                hoursString = "0" + hoursString;
+            }
+
+            String minutesString = minutes + "m ";
+            if (remainingMinutes < 10) {
+                minutesString = "0" + minutesString;
+            }
+
+            String secondsString = seconds + "s";
+            if (seconds < 10) {
+                secondsString = "0" + secondsString;
+            }
+
+            return hoursString  + minutesString +  secondsString ;
         }
     }
 
@@ -23,10 +41,23 @@ public class Main {
 
         if (seconds < 0) {
             return "Invalid Value";
+
         } else {
+
             long minutes = seconds / 60;
             long remainingSeconds = seconds % 60;
-            return minutes + "m " + remainingSeconds + "s";
+
+            String minutesString = minutes + "m ";
+            if (minutes < 10) {
+                minutesString = "0" + minutesString;
+            }
+
+            String secondsString = remainingSeconds + "s";
+            if (remainingSeconds < 10) {
+                secondsString = "0" + secondsString;
+            }
+
+            return minutesString + secondsString ;
         }
     }
 
