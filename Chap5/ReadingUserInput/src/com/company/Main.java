c87dd4f5e79ed13ecc8769bf0293018af7acc53a
Anjali <anjali.payal69@gmail.com>
2020-09-06 18:32:39 +0200

    Reading User Input Solutions and Problems

--- Chap5/ReadingUserInput/src/com/company/Main.java
+++ Chap5/ReadingUserInput/src/com/company/Main.java
@@ -8,16 +8,27 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine(); // handle next line character (enter key)
 
-        System.out.println("Enter you name: ");
-        String name = scanner.nextLine();
+        boolean hasNextInt = scanner.hasNextInt();
 
-        int age = 2020 - yearOfBirth;
+        if (hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // handle next line character (enter key)
 
-        System.out.println("Your name is " + name);
-        System.out.println("You are " + age + " years old");
+            System.out.println("Enter you name: ");
+            String name = scanner.nextLine();
+
+            int age = 2020 - yearOfBirth;
+
+            if (age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name);
+                System.out.println("You are " + age + " years old");
+            } else {
+                System.out.println("Invalid year of birth");
+            }
+        } else {
+            System.out.println("Unable to parse year of birth.");
+        }
 
         scanner.close();
 
