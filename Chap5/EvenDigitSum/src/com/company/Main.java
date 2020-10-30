a0917d284e0c327371c7d9168320f4057f278935
Anjali <anjali.payal69@gmail.com>
2020-10-30 11:35:01 +0200

    Coding Exercise - Even Digit Sum

---
+++ Chap5/EvenDigitSum/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(123456789));
+        System.out.println(getEvenDigitSum(252));
+        System.out.println(getEvenDigitSum(-22));
+    }
+
+    public static int getEvenDigitSum(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int sum = 0;
+
+        while (number > 0 ) {
+            int lastDigit = number % 10;
+
+            if (lastDigit % 2 == 0) {
+                sum += lastDigit;
+                number /= 10;
+            } else {
+                lastDigit = 0;
+                number /= 10;
+            }
+        }
+        return sum;
+    }
+}
