08fa88e6379493abf9c40817f351b9e4cb05c465
Anjali <anjali.payal69@gmail.com>
2020-10-31 09:21:45 +0200

    Coding Exercise - Flour Pack Problem

---
+++ Chap5/FlourPackProblem/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(canPack(1,0,4));
+        System.out.println(canPack(1,0,5));
+        System.out.println(canPack(0,5,4));
+        System.out.println(canPack(2,2,11));
+        System.out.println(canPack(-3,2,12));
+        System.out.println(canPack(2,1,5));
+    }
+
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+        if ((bigCount < 0) || (smallCount < 0) || (goal < 0)) {
+            return false;
+        }
+
+        int bigBag  = bigCount * 5;
+
+        if (bigBag + smallCount > goal && goal % 5 <= smallCount) {
+            return true;
+        } else if (goal == bigBag + smallCount) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
