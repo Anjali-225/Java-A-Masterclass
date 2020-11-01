2a64cfd30aa0cac0db204545206444eb8ea1141a
Anjali <anjali.payal69@gmail.com>
2020-11-01 16:15:40 +0200

    Coding Exercise - Diagoal Star

---
+++ Chap5/DiagonalStar/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printSquareStar(5);
+        printSquareStar(8);
+    }
+
+    public static void printSquareStar(int number){
+        if (number < 5) {
+            System.out.println("Invalid Value");
+        } else {
+            for (int row = 1; row <= number; row++) {
+                for (int col = 1; col <= number; col++){
+                    if (row == 1 || col == 1 || row == number || col == number || col == row || row + col == number + 1) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
