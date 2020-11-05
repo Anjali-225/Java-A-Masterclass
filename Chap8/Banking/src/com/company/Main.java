7cb64f9f83e63b3dd093c5b10f539f2dcab42e55
Anjali <anjali.payal69@gmail.com>
2020-11-05 18:56:22 +0200

    Autoboxing and Unboxing Challenge Part 3

--- Chap8/Banking/src/com/company/Main.java
+++ Chap8/Banking/src/com/company/Main.java
@@ -3,6 +3,43 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Bank bank = new Bank("Nedbank");
+
+        if (bank.addBranch("Rivonia")){
+            System.out.println("Rivonia branch created");
+        }
+
+	    bank.addBranch("Rivonia");
+	    bank.addCustomer("Rivonia", "Tim", 174.34);
+        bank.addCustomer("Rivonia", "Mike", 50.50);
+        bank.addCustomer("Rivonia", "Percy", 220.12);
+
+        bank.addBranch("Secunda");
+        bank.addCustomer("Secunda", "Bob", 15.12);
+
+        bank.addCustomerTransaction("Rivonia", "Tim", 44.22);
+        bank.addCustomerTransaction("Rivonia", "Tim", 12.44);
+        bank.addCustomerTransaction("Rivonia", "Mike", 1.65);
+
+        bank.listCustomers("Rivonia", true);
+        bank.listCustomers("Secunda", true);
+
+        bank.addBranch("Woodmead");
+
+        if (!bank.addCustomer("Woodmead", "Brown", 5.33)){
+            System.out.println("Error branch does not exist");
+        }
+
+        if (!bank.addBranch("Rivonia")){
+            System.out.println("Rivonia branch already exists");
+        }
+
+        if (!bank.addCustomerTransaction("Rivonia", "Fergus", 52.33)){
+            System.out.println("Customer does not exist at branch");
+        }
+
+        if(!bank.addCustomer("Rivonia", "Tim", 12.21)) {
+            System.out.println("Customer Tim already exists");
+        }
     }
 }
