9c764d6f3c3cad7e6d4089f5d8e7fc752fd65d97
Anjali <anjali.payal69@gmail.com>
2020-11-09 17:51:15 +0200

    Our Generics Class Part 2

--- Chap10/GenericsClass/src/com/company/Main.java
+++ Chap10/GenericsClass/src/com/company/Main.java
@@ -7,11 +7,30 @@ public class Main {
         BaseballPlayer pat = new BaseballPlayer("Pat");
         SoccerPlayer becky = new SoccerPlayer("Becky");
 
-        Team adelaideCrows = new Team("Adelaide Crows");
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
         adelaideCrows.addPlayer(joe);
-        adelaideCrows.addPlayer(pat);
-        adelaideCrows.addPlayer(becky);
+//        adelaideCrows.addPlayer(pat);
+//        adelaideCrows.addPlayer(becky);
 
         System.out.println(adelaideCrows.numPlayers());
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Clubs");
+        baseballTeam.addPlayer(pat);
+
+        Team<SoccerPlayer> brokenTeam = new Team<>("this won't work");
+        brokenTeam.addPlayer(becky);
+
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        FootballPlayer banks = new FootballPlayer("Gordon");
+        melbourne.addPlayer(banks);
+
+        Team<FootballPlayer> hawthorn= new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle= new Team<>("Fremantle");
+
+        hawthorn.matchResult(fremantle, 1, 0);
+        hawthorn.matchResult(adelaideCrows, 3, 8);
+
+        adelaideCrows.matchResult(fremantle, 2, 1);
+//        adelaideCrows.matchResult(baseballTeam, 1, 1);
     }
 }
