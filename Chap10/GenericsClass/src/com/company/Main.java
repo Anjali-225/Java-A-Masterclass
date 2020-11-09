527d0800ad7c4a4f29b9ee80ce0381d9f4357190
Anjali <anjali.payal69@gmail.com>
2020-11-09 17:26:21 +0200

    Our Generics Class

---
+++ Chap10/GenericsClass/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer becky = new SoccerPlayer("Becky");
+
+        Team adelaideCrows = new Team("Adelaide Crows");
+        adelaideCrows.addPlayer(joe);
+        adelaideCrows.addPlayer(pat);
+        adelaideCrows.addPlayer(becky);
+
+        System.out.println(adelaideCrows.numPlayers());
+    }
+}
