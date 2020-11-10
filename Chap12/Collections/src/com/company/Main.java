cf6e48da99c125b75bfdb44165a3f3d2cacc39ad
Anjali <anjali.payal69@gmail.com>
2020-11-10 17:01:37 +0200

    Collections Overview

---
+++ Chap12/Collections/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Theatre theatre = new Theatre("Ster Kinekor", 8, 12);
+        theatre.getSeats();
+
+        if (theatre.reserveSeat("H11")) {
+            System.out.println("Please Pay");
+        } else {
+            System.out.println("Sorry seat is taken");
+        }
+
+        if (theatre.reserveSeat("H11")) {
+            System.out.println("Please Pay");
+        } else {
+            System.out.println("Sorry seat is taken");
+        }
+
+
+    }
+}
