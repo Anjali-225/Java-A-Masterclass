6784f8ff72eb937f1db078050aaa0229a3b73255
Anjali <anjali.payal69@gmail.com>
2020-11-12 09:34:49 +0200

    TreeMap and Unmodifiable Maps

--- Chap12/SortedCollections/src/com/company/Main.java
+++ Chap12/SortedCollections/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Map;
+
 public class Main {
 
     private static StockList stockList = new StockList();
@@ -18,6 +20,9 @@ public class Main {
         stockList.addStock(temp);
 
         temp = new StockItem("cup", 3.10, 6);
+        stockList.addStock(temp);
+
+        temp = new StockItem("cup", 0.45, 10);
         stockList.addStock(temp);
 
         temp = new StockItem("door", 25.65, 3);
@@ -40,5 +45,55 @@ public class Main {
         for (String s : stockList.Items().keySet()) {
             System.out.println(s);
         }
+
+        Basket myBasket = new Basket("Mine");
+        sellItem(myBasket, "car", 1);
+        System.out.println(myBasket);
+
+        sellItem(myBasket, "car", 1);
+        System.out.println(myBasket);
+
+        if (sellItem(myBasket, "car", 1) != 1) {
+            System.out.println("There are no more cars in stock");
+        }
+        System.out.println(myBasket);
+
+        sellItem(myBasket, "spanner", 5);
+        System.out.println(myBasket);
+
+        sellItem(myBasket, "juice", 4);
+        sellItem(myBasket, "cup", 12);
+        sellItem(myBasket, "bread", 1);
+        System.out.println(myBasket);
+
+        System.out.println(stockList);
+
+        // This will give us an unsupported operation, because its an unmodifiable map
+//        temp = new StockItem("pen", 1.12);
+//        stockList.Items().put(temp.getName(), temp);
+
+        stockList.Items().get("car").adjustStock(100);
+        stockList.get("car").adjustStock(-10);
+        System.out.println(stockList);
+
+        for (Map.Entry<String, Double> price : stockList.PriceList().entrySet()) {
+            System.out.println(price.getKey() + " costs " + price.getValue());
+        }
+
+    }
+
+    public static int sellItem(Basket basket, String item, int quantity) {
+        // retrieve the item from stock list
+        StockItem stockItem = stockList.get(item);
+        if (stockItem == null ){
+            System.out.println("We don't sell " + item);
+            return 0;
+        }
+
+        if (stockList.sellStock(item, quantity) != 0) {
+            basket.addToBasket(stockItem, quantity);
+            return quantity;
+        }
+        return 0;
     }
 }
--- Chap12/SortedCollections/src/com/company/StockList.java
+++ Chap12/SortedCollections/src/com/company/StockList.java
@@ -40,6 +40,14 @@ public class StockList {
         return list.get(key);
     }
 
+    public Map<String, Double> PriceList() {
+        Map<String, Double> prices = new LinkedHashMap<>();
+        for (Map.Entry<String, StockItem> item : list.entrySet()) {
+            prices.put(item.getKey(), item.getValue().getPrice());
+        }
+        return Collections.unmodifiableMap(prices);
+    }
+
     public Map<String, StockItem> Items() {
         return Collections.unmodifiableMap(list);
     }
@@ -57,6 +65,6 @@ public class StockList {
             s = s + String.format("%.2f",itemValue) + "\n";
             totalCost += itemValue;
         }
-        return s + " Total stock value " + totalCost;
+        return s + "Total stock value " + totalCost;
     }
 }
